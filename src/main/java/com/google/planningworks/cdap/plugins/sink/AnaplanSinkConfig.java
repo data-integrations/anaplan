95b8b05a96cd5a79493602d6bd383c8350c6ed25
niceyang <melosz@google.com>
2021-04-27 18:28:31 -0700

    The initial commit for the Anaplan plugin (0.0.1-snapshot), including: 1. Anaplan library code from repo(https://github.com/anaplaninc/anaplan-java-client) 2. Wrapper class of the Anaplan service required by the plugins (AnaplanService.java) 3. Anaplan Function runner plugin 4. Anaplan Sink plugin 5. Anaplan Source plugin
    * Anaplan Source plugin is built upon GCS plugin in repo(https://github.com/data-integrations/google-cloud/blob/develop/src/main/java/io/cdap/plugin/gcp/gcs/source/GCSSource.java)
    * GCSPath.java is copied for supporting the Anaplan Source plugin from repo (https://github.com/data-integrations/google-cloud/tree/develop/src/main/java/io/cdap/plugin/gcp/gcs)

---
+++ src/main/java/com/google/planningworks/cdap/plugins/sink/AnaplanSinkConfig.java
@@ -0,0 +1,168 @@
+package com.google.planningworks.cdap.plugins.sink;
+
+import static com.anaplan.client.AnaplanService.NAME_AUTH_SERVICE_LOCATION;
+import static com.anaplan.client.AnaplanService.NAME_MODEL_ID;
+import static com.anaplan.client.AnaplanService.NAME_PASSWORD;
+import static com.anaplan.client.AnaplanService.NAME_SERVER_FILE_NAME;
+import static com.anaplan.client.AnaplanService.NAME_SERVICE_LOCATION;
+import static com.anaplan.client.AnaplanService.NAME_USERNAME;
+import static com.anaplan.client.AnaplanService.NAME_WORKSPACE_ID;
+
+import com.anaplan.client.AnaplanService;
+import com.anaplan.client.AnaplanService.AnaplanConfig;
+import com.google.common.base.Strings;
+import io.cdap.cdap.api.annotation.Description;
+import io.cdap.cdap.api.annotation.Macro;
+import io.cdap.cdap.api.annotation.Name;
+import io.cdap.cdap.api.plugin.PluginConfig;
+import io.cdap.cdap.etl.api.FailureCollector;
+
+/**
+ * The config class for {@link AnaplanSink} that contains all properties that need to be filled in
+ * by the user when building a pipeline.
+ */
+class AnaplanSinkConfig extends PluginConfig implements AnaplanConfig {
+  public static final int CHUNK_SIZE = 10;
+
+  @Name(NAME_SERVICE_LOCATION)
+  @Macro
+  @Description("Service location of your Anaplan API")
+  private String serviceLocation;
+
+  @Name(NAME_AUTH_SERVICE_LOCATION)
+  @Macro
+  @Description("Service location of your Anaplan authentication API")
+  private String authServiceLocation;
+
+  @Name(NAME_USERNAME)
+  @Macro
+  @Description("Username")
+  private String username;
+
+  @Name(NAME_PASSWORD)
+  @Macro
+  @Description("Password")
+  private String password;
+
+  @Name(NAME_WORKSPACE_ID)
+  @Macro
+  @Description("WorkspaceId")
+  private String workspaceId;
+
+  @Name(NAME_MODEL_ID)
+  @Macro
+  @Description("ModelId")
+  private String modelId;
+
+  @Name(NAME_SERVER_FILE_NAME)
+  @Macro
+  @Description("Anaplan Server file name")
+  private String serverFileName;
+
+  /**
+   * Validates the configure options entered by the user
+   *
+   * @param collector the shared failure info collector storing the validation failures
+   */
+  public void validate(FailureCollector collector) {
+    if (!containsMacro(NAME_SERVICE_LOCATION)) {
+      try {
+        AnaplanService.validateServiceLocation(serviceLocation);
+      } catch (IllegalArgumentException e) {
+        collector
+            .addFailure(e.getMessage(), null)
+            .withConfigProperty(NAME_SERVICE_LOCATION)
+            .withStacktrace(e.getStackTrace());
+      }
+    }
+
+    if (!containsMacro(NAME_AUTH_SERVICE_LOCATION)) {
+      try {
+        AnaplanService.validateAuthServiceLocation(authServiceLocation);
+      } catch (IllegalArgumentException e) {
+        collector
+            .addFailure(e.getMessage(), null)
+            .withConfigProperty(NAME_AUTH_SERVICE_LOCATION)
+            .withStacktrace(e.getStackTrace());
+      }
+    }
+
+    if (!containsMacro(NAME_USERNAME)) {
+      try {
+        AnaplanService.validateUsername(username);
+      } catch (IllegalArgumentException e) {
+        collector
+            .addFailure(e.getMessage(), null)
+            .withConfigProperty(NAME_USERNAME)
+            .withStacktrace(e.getStackTrace());
+      }
+    }
+
+    if (!containsMacro(NAME_PASSWORD)) {
+      try {
+        AnaplanService.validatePassword(password);
+      } catch (IllegalArgumentException e) {
+        collector
+            .addFailure(e.getMessage(), null)
+            .withConfigProperty(NAME_PASSWORD)
+            .withStacktrace(e.getStackTrace());
+      }
+    }
+
+    if (!containsMacro(NAME_WORKSPACE_ID)) {
+      try {
+        AnaplanService.validateWorkspaceId(workspaceId);
+      } catch (IllegalArgumentException e) {
+        collector
+            .addFailure(e.getMessage(), null)
+            .withConfigProperty(NAME_WORKSPACE_ID)
+            .withStacktrace(e.getStackTrace());
+      }
+    }
+
+    if (!containsMacro(NAME_MODEL_ID)) {
+      try {
+        AnaplanService.validateModelId(modelId);
+      } catch (IllegalArgumentException e) {
+        collector
+            .addFailure(e.getMessage(), null)
+            .withConfigProperty(NAME_MODEL_ID)
+            .withStacktrace(e.getStackTrace());
+      }
+    }
+
+    if (!containsMacro(NAME_SERVER_FILE_NAME) && Strings.isNullOrEmpty(serverFileName)) {
+      collector
+          .addFailure("Server file is not presented.", null)
+          .withConfigProperty(NAME_MODEL_ID);
+    }
+  }
+
+  public String getServiceLocation() {
+    return serviceLocation;
+  }
+
+  public String getAuthServiceLocation() {
+    return authServiceLocation;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public String getWorkspaceId() {
+    return workspaceId;
+  }
+
+  public String getModelId() {
+    return modelId;
+  }
+
+  public String getServerFileName() {
+    return serverFileName;
+  }
+}
